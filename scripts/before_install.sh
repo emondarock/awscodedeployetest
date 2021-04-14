80ec0f910189a39e7afc4a92cccdc475c7c3a914
emondarock@gmail.com <3m0nd4r0ck>
2021-04-14 06:08:21 +0600

    Next V6

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,4 @@
 sudo apt-get update
 sudo mkdir /home/ubuntu/newdir
-cd /home/ubuntu/
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
 cd /home/ubuntu/app/
 sudo rm -rf /node_modules
